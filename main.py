0e20b9ecb70869f18efb04a30992b09cbe3c47de
SvennieboyDev <Sven.Deijkers@gmail.com>
2024-10-15 20:13:42 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#Gestart op 15 oktober 2024
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        self.title = "Time Spent On"
+        layout = GridLayout(cols=2)
+        layout.add_widget(Label(text="Time Spent On"))
+        layout.add_widget(Label(text="15 October 2024"))
+        return layout
+    
+if __name__ == "__name__":
+    MyApp().run()
