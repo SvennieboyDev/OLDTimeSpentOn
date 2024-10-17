d81298a0720630a1b82284339701b2aebe289245
SvennieboyDev <Sven.Deijkers@gmail.com>
2024-10-17 22:35:44 +0200

    Time Spent On 0.1.0
    17-10-2024

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 #Gestart op 15 oktober 2024
 
+import kivy
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
-class MyApp(App):
+class StartWindow(Screen):
+    pass
+
+class MainWindow(Screen):
+    timeCount = NumericProperty(0)
+
+    time = ObjectProperty(None)
+
+    def addTime(self):
+        self.timeCount += int(self.time.text)
+        print("Time: ", self.timeCount)
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("timespenton.kv")
+
+class TimeSpentOn(App):
     def build(self):
         self.title = "Time Spent On"
-        layout = GridLayout(cols=2)
-        layout.add_widget(Label(text="Time Spent On"))
-        layout.add_widget(Label(text="15 October 2024"))
-        return layout
+        return kv
     
 if __name__ == "__main__":
-    MyApp().run()
+    TimeSpentOn().run()
