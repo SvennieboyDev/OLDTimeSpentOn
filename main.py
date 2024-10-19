7179560f19262372e9c04f1561666ca994fecd57
SvennieboyDev <Sven.Deijkers@gmail.com>
2024-10-19 13:52:37 +0200

    Create button fixed
    19-10-2024

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class StartWindow(Screen):
     pass
@@ -31,8 +31,13 @@ class MainWindow(Screen):
             pass
     
     def create(self):
-        new_subject = Label(text="test")
+        new_subject = Label(text="New Subject")
+        new_subject_timeInput = TextInput(text="test", multiline=False)
+        new_subject_addTime = Button(text="Add time")
+        new_subject_showtime = Label(text="Time: ")
+
         self.widget_container.add_widget(new_subject)
+        self.widget_container.add_widget(new_subject_timeInput)
 
 
 class WindowManager(ScreenManager):
@@ -42,7 +47,7 @@ kv = Builder.load_file("timespenton.kv")
 
 class TimeSpentOn(App):
     def build(self):
-        Window.maximize()
+        #Window.maximize()
 
         self.title = "Time Spent On"
         return kv
